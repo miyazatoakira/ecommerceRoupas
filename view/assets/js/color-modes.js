827feeca1e4c678777f22c01c1b2ec77473dc269
miyazatoakira <augusto.akira08@hotmail.com>
2024-08-03 15:24:26 -0300

    Navbar, Logo e Switch Theme adicionado

--- view/assets/js/color-modes.js
+++ view/assets/js/color-modes.js
@@ -1,90 +1,99 @@
 (() => {
   'use strict'
 
-  const getStoredTheme = () => localStorage.getItem('theme')
-  const setStoredTheme = theme => localStorage.setItem('theme', theme)
+  const getStoredTheme = () => localStorage.getItem('theme');
+  const setStoredTheme = theme => localStorage.setItem('theme', theme);
 
   const getPreferredTheme = () => {
-    const storedTheme = getStoredTheme()
+    const storedTheme = getStoredTheme();
     if (storedTheme) {
-      return storedTheme
+      return storedTheme;
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   }
 
-  
-    const updateLogo = (theme) => {
-      const logo = document.getElementById('logo-brand')
-      const navbar = document.querySelector('.navbar')
-      if (!logo) return
-  
-      if (theme === 'dark' || (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        logo.src = './assets/images/navbar-brand-dark.png'
-        navbar.style.backgroundColor = '#212529';
-      } else {
-        logo.src = './assets/images/navbar-brand-light.png'
-        navbar.style.backgroundColor = '#ffffff';
+  const updateLogo = (theme) => {
+    const logo = document.getElementById('logo-brand');
+    const navbar = document.querySelector('.navbar');
+    const moon = document.querySelector('#moon-stars-fill');
+    const sun = document.querySelector('#sun-fill');
 
-      }
+    if (!logo) return;
+
+    if (theme === 'dark' || (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      logo.src = './assets/images/navbar-brand-dark.png';
+      navbar.style.backgroundColor = '#212529';
+      moon.style.color = '#000000';
+      sun.style.color = '#000000';
+
+    } else {
+      logo.src = './assets/images/navbar-brand-light.png';
+      navbar.style.backgroundColor = '#ffffff';
+      moon.style.color = '#ffffff';
+      sun.style.color = '#ffffff';
+
+
     }
+  }
+
   const setTheme = theme => {
     if (theme === 'auto') {
-      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      document.documentElement.setAttribute('data-bs-theme', theme);
     }
-    updateLogo(theme)
+    updateLogo(theme); 
   }
 
-  setTheme(getPreferredTheme())
-
   const showActiveTheme = (theme, focus = false) => {
-    const themeSwitcher = document.querySelector('#bd-theme')
+    const themeSwitcher = document.querySelector('#bd-theme');
 
     if (!themeSwitcher) {
-      return
+      return;
     }
 
-    const themeSwitcherText = document.querySelector('#bd-theme-text')
-    const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+    const themeSwitcherText = document.querySelector('#bd-theme-text');
+    const activeThemeIcon = document.querySelector('.theme-icon-active use');
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href');
 
     document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      element.classList.remove('active')
-      element.setAttribute('aria-pressed', 'false')
-    })
+      element.classList.remove('active');
+      element.setAttribute('aria-pressed', 'false');
+    });
 
-    btnToActive.classList.add('active')
-    btnToActive.setAttribute('aria-pressed', 'true')
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-    themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+    btnToActive.classList.add('active');
+    btnToActive.setAttribute('aria-pressed', 'true');
+    activeThemeIcon.setAttribute('href', svgOfActiveBtn);
+    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+    themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
 
     if (focus) {
-      themeSwitcher.focus()
+      themeSwitcher.focus();
     }
   }
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    const storedTheme = getStoredTheme()
+    const storedTheme = getStoredTheme();
     if (storedTheme !== 'light' && storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
+      setTheme(getPreferredTheme());
     }
-  })
+  });
 
   window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
+    const preferredTheme = getPreferredTheme();
+    setTheme(preferredTheme);
+    showActiveTheme(preferredTheme);
 
     document.querySelectorAll('[data-bs-theme-value]')
       .forEach(toggle => {
         toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-bs-theme-value')
-          setStoredTheme(theme)
-          setTheme(theme)
-          showActiveTheme(theme, true)
-        })
-      })
-  })
-})()
+          const theme = toggle.getAttribute('data-bs-theme-value');
+          setStoredTheme(theme);
+          setTheme(theme);
+          showActiveTheme(theme, true);
+        });
+      });
+  });
+})();
