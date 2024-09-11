10dd1f93b73af82c0768a08db5dea07c1ede9ac9
Rian-junior <rian.junio25@gmail.com>
2024-09-11 12:36:33 -0300

    Conexao PHP

---
+++ lojavenda.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nomeUsu = $_POST['nomeusu'];
+    $cpf = $_POST['CPF'];
+    $tel = $_POST['tel'];
+    $email = $_POST['email'];
+    $resid = $_POST['resid'];
+    $comp = $_POST['comp'];
+    $obs = $_POST['obs'];
+    $tamprod = $_POST[''];
+    $tipoprod = $_POST[''];
+    $cep = $_POST[''];
+    $rua = $_POST[''];
+    $bairro = $_POST[''];
+    $cidade = $_POST[''];
+    $uf = $_POST[''];
+
+
+?> 
--- model/functions.php
+++ model/functions.php
@@ -1,1 +1,28 @@
-public c
+<?php
+
+Class Conexao{
+    private $pdo;
+    private $host = "localhost";
+    private $dbname = "LojaRoupas";
+    private $user = "root";
+    private $senha = "";
+
+        public function __construct(){
+            try{
+                $this->pdo = new PDO("myskl:dbname=".$this->dbname.";host=".$this->host
+                ,$this->user, $this->senha);
+            }
+            catch (PDOException $e){
+                echo "ERRO DE CONEXÃO NO PDO: ".$e->getMessage();
+                exit();
+            }
+            catch(Exception $e){
+                echo "ERRO não passou de conexão: ".$e->getMessage();
+                exit();
+            }
+            return $this->pdo;
+        }
+
+
+}
+?>
